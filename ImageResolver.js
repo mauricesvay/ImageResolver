6d951f5101e80fdaddb85428787dce7b0766ebae
Maurice Svay <maurice@svay.com>
2012-07-15 00:08:03 +0200

    Demo

---
+++ ImageResolver.js
@@ -0,0 +1,236 @@
+window.ImageResolver = (function(){
+
+    function ImageResolver() {
+        this.filters = [];
+    }
+
+    ImageResolver.prototype.register = function(fn) {
+        this.filters.push(fn);
+        return this;
+    };
+
+    ImageResolver.prototype.next = function(filters, url, clbk) {
+        var self = this;
+        var filter;
+        if (filters.length) {
+            filter = filters[0];
+            filter.resolve(url, function(data){
+                if (data === null) {
+                    self.next(filters.slice(1), url, clbk);
+                    return;
+                } else {
+                    clbk(data);
+                    return;
+                }
+            });
+            return;
+        }
+        // console.log('No image found on ' + url);
+        clbk(null);
+    };
+
+    ImageResolver.prototype.resolve = function(url, clbk) {
+        var filters = this.filters;
+        this.next(filters, url, clbk);
+        return this;
+    };
+
+    return new ImageResolver();
+
+})();
+
+/**
+ * Plugins
+ */
+
+/**
+ * filter by file extension
+ */
+
+function FileExtensionResolver() {
+}
+FileExtensionResolver.prototype.resolve = function(url, clbk) {
+    var pathname = URI(url).pathname();
+    if (pathname.match(/(png|jpg|jpeg|gif|bmp)$/i)) {
+        clbk(url);
+        return;
+    }
+    clbk(null);
+};
+
+/**
+ * Imgur page
+ */
+function ImgurPageResolver() {
+}
+ImgurPageResolver.prototype.resolve = function(url, clbk) {
+    var matches = url.match(/http:\/\/imgur.com\/(.*)/) || [];
+    if (matches.length && (-1 === matches[1].indexOf('/'))) {
+        clbk('http://i.imgur.com/' + matches[1] + '.jpg');
+        return;
+    }
+    clbk(null);
+};
+
+/**
+ * Imgur album
+ */
+function ImgurAlbumResolver() {
+}
+ImgurAlbumResolver.prototype.resolve = function(url, clbk) {
+    var matches = url.match(/http:\/\/imgur.com\/a\/(.*)/) || [];
+    var api;
+    if (matches.length) {
+        api = 'http://api.imgur.com/2/album/' + matches[1] + '.json';
+        $.ajax({
+            url : api,
+            method: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                var images = data.album.images || [];
+                if (images.item && images.item.length) {
+                    images = images.item;
+                }
+                var first = images.shift();
+                if (first) {
+                    clbk(first.links.large_thumbnail);
+                    return;
+                }
+                clbk(null);
+                return;
+            }
+        });
+        return;
+    }
+    clbk(null);
+    return;
+};
+
+/**
+ * 9gag page
+ */
+function NineGagResolver() {
+}
+NineGagResolver.prototype.resolve = function(url, clbk) {
+    var matches = url.match(/http:\/\/9gag.com\/gag\/(.*)/) || [];
+    var id;
+    var image;
+    if (matches.length) {
+        id = matches[1];
+        image = 'http://d24w6bsrhbeh9d.cloudfront.net/photo/' + id + '_700b.jpg';
+        clbk(image);
+        return;
+    }
+    clbk(null);
+};
+
+/**
+ * Flickr photo page
+ */
+function FlickrResolver(apikey) {
+    this.apikey = apikey;
+}
+FlickrResolver.prototype.resolve = function(url, clbk) {
+    var matches = url.match(/http:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/(.*)/) || [];
+    var id;
+    var image;
+    var api;
+    var tplApi = 'http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key={{APIKEY}}&photo_id={{ID}}&format=json&jsoncallback=?';
+    if (matches.length) {
+        id = matches[2];
+        api = tplApi
+            .replace('{{APIKEY}}', this.apikey)
+            .replace('{{ID}}', id);
+        $.ajax({
+            method: 'GET',
+            url: api,
+            dataType: 'jsonp',
+            success: function(data) {
+                if (data && data.sizes && data.sizes.size) {
+                    var sizes = data.sizes.size;
+                    for (var i=0, l=sizes.length; i<l; i++) {
+                        if (sizes[i].label === 'Large') {
+                            clbk(sizes[i].source);
+                            return;
+                        }
+                    }
+                }
+                clbk(null);
+                return;
+            }
+        });
+        return;
+    }
+    clbk(null);
+    return;
+};
+
+/**
+ * Any web page
+ */
+function WebpageResolver() {
+}
+//Good-enough tag parser
+//Doesn't support attr with no value
+//Doesn't support values with = (equal character)
+WebpageResolver.prototype._parseTag = function(tag) {
+    tag = tag.replace('/>', '>');
+    tag = tag.replace('>','');
+    tag = tag.replace('<','');
+    var tagname = tag.substr(0, tag.indexOf(' '));
+    var attr = tag.match(/(\S+)=["']?([^"']*)["']?/g);
+    var attributes = {};
+    var parts;
+    for (var i=0,l=attr.length; i<l; i++) {
+        parts = attr[i].split('=');
+        attributes[parts[0].toLowerCase()] = parts[1].replace(/^["']/,'').replace(/["']$/, '');
+    }
+    return {
+        tag: tagname,
+        attributes: attributes
+    };
+};
+WebpageResolver.prototype.resolve = function(url, clbk) {
+    var self = this;
+    $.ajax({
+        url : url,
+        method: 'GET',
+        dataType: 'text',
+        success: function(html) {
+            var images = html.match(/<img([^>]*)>/g) || [];
+            var image;
+            var candidates = [];
+            var tag;
+            for (var i=0,l=images.length; i<l; i++) {
+                tag = self._parseTag(images[i]);
+                if (tag.attributes.src && tag.attributes.width && tag.attributes.height) {
+                    candidates.push({
+                        url: tag.attributes.src,
+                        surface: tag.attributes.width * tag.attributes.height
+                    });
+                }
+            }
+
+            if (candidates.length) {
+                candidates = candidates.sort(function(a,b){
+                    return b.surface - a.surface;
+                });
+                image = candidates[0].url;
+
+                //Resolve relative url
+                if (!image.match(/^http/)) {
+                    if (image.match(/^\//)) {
+                        image = URI(url).pathname(image).toString();
+                    } else {
+                        image = URI(url).filename(image).toString();
+                    }
+                }
+
+                clbk(image);
+                return;
+            }
+            clbk(null);
+            return;
+        }
+    });
+};
---
+++ README.md
@@ -0,0 +1,53 @@
+ImageResolver.js
+================
+
+ImageResolver.js does its best to determine the main image on a URL **without
+loading all images**.
+
+Works without any extra request with:
+
+* image URLs
+* imgur.com photo pages
+* 9gag
+
+Works with an additional request:
+
+* imgur.com album pages
+* flickr.com photo pages (requires an API key)
+* any other webpage (requires a CORS proxy)
+
+ImageResolvers.js compatibility can be extended to more sites by writing plugins.
+
+Usage
+-----
+The API might break in the future.
+
+    <script src="ImageResolver.js" type="text/javascript"></script>
+    <script>
+    ImageResolver.register(new FileExtensionResolver());
+    ImageResolver.register(new ImgurPageResolver());
+    ImageResolver.register(new NineGagResolver());
+    ImageResolver.resolve(url, callback);
+    </script>
+
+
+Dependencies
+------------
+I'm working to reduce dependencies, but for now you need:
+
+* jQuery
+* URI.js
+
+License
+-------
+
+This app is under the BSD license:
+
+    Copyright (c) 2012, Maurice Svay
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
