54ce2c9cfb0e9dffdf1eba1586ca8f6cc028cf54
Maurice Svay <maurice@svay.com>
2013-05-02 12:36:36 +0200

    Add HackerNews feed example

---
+++ example/utils.js
@@ -0,0 +1,9 @@
+function extractUrl(str) {
+    var uri_pattern = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
+    var result = uri_pattern.exec(str);
+    // console.log(result);
+    if (!result) {
+        return [];
+    }
+    return result;
+}
