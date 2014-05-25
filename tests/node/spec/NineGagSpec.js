2617823f49b27737edfc908ccced8bb455b4a98f
Maurice Svay <maurice@svay.com>
2014-05-25 12:09:46 +0200

    Update NineGag plugin tests

--- tests/node/spec/NineGagSpec.js
+++ tests/node/spec/NineGagSpec.js
@@ -1,57 +1,42 @@
 var ImageResolver = require('../../../src/ImageResolver');
-var imageresolver = new ImageResolver();
-imageresolver.register(new ImageResolver.NineGag());
+var NineGag = new ImageResolver.NineGag();
 
-describe("NineGag : Pirate Dog", function(){
-    it("should return the jpg URL", function(){
+describe("NineGag : not a 9gag url", function() {
+    it("should return null", function(){
         var result;
-        imageresolver.resolve(
-            "http://9gag.com/gag/4760092",
-            function(res){
-                result = res;
-            }
-        );
-        waitsFor(function(){
-            return result;
-        }, 10000);
-        runs(function(){
-            expect(result.image).toBe("http://d24w6bsrhbeh9d.cloudfront.net/photo/4760092_700b.jpg");
+        NineGag.resolve("http://example.com/", function( res ){
+            result = res;
         });
+        expect(result).toBe(null);
     });
 });
 
-describe("NineGag : Subcategory Girl", function(){
-    it("should return the jpg URL", function(){
+describe("NineGag : 9gag url", function() {
+    it("should return url", function(){
         var result;
-        imageresolver.resolve(
-            "http://9gag.com/gag/a75mmEb?sc=girl",
-            function(res){
-                result = res;
-            }
-        );
-        waitsFor(function(){
-            return result;
-        }, 10000);
-        runs(function(){
-            expect(result.image).toBe("http://d24w6bsrhbeh9d.cloudfront.net/photo/a75mmEb_700b.jpg");
+        NineGag.resolve("http://9gag.com/gag/4760092", function( res ){
+            result = res;
         });
+        expect(result).toBe("http://d24w6bsrhbeh9d.cloudfront.net/photo/4760092_700b.jpg");
     });
 });
 
-describe("NineGag : Subcategory Comic", function(){
-    it("should return the jpg URL", function(){
+describe("NineGag : 9gag Girls url", function() {
+    it("should return url", function(){
         var result;
-        imageresolver.resolve(
-            "http://9gag.com/gag/aeNwN8B?sc=comic",
-            function(res){
-                result = res;
-            }
-        );
-        waitsFor(function(){
-            return result;
-        }, 10000);
-        runs(function(){
-            expect(result.image).toBe("http://d24w6bsrhbeh9d.cloudfront.net/photo/aeNwN8B_700b.jpg");
+        NineGag.resolve("http://9gag.com/gag/a75mmEb?sc=girl", function( res ){
+            result = res;
         });
+        expect(result).toBe("http://d24w6bsrhbeh9d.cloudfront.net/photo/a75mmEb_700b.jpg");
+    });
+});
+
+describe("NineGag : 9gag Comics url", function() {
+    it("should return url", function(){
+        var result;
+        NineGag.resolve("http://9gag.com/gag/aeNwN8B?sc=comic", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://d24w6bsrhbeh9d.cloudfront.net/photo/aeNwN8B_700b.jpg");
     });
 });
