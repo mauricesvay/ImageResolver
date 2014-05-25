bca4fe752523112295c9e0e476496517dd4eed10
Maurice Svay <maurice@svay.com>
2014-05-25 11:48:13 +0200

    Update FileExtension tests

--- tests/node/spec/FileExtensionSpec.js
+++ tests/node/spec/FileExtensionSpec.js
@@ -1,57 +1,133 @@
 var ImageResolver = require('../../../src/ImageResolver');
-var imageresolver = new ImageResolver();
-imageresolver.register(new ImageResolver.FileExtension());
+var FileExtension = new ImageResolver.FileExtension();
 
-describe("FileExtension : jpg", function(){
-    it("should return the jpg URL as is", function(){
+describe("FileExtension : not an image url", function() {
+    it("should return null", function(){
         var result;
-        imageresolver.resolve(
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/250px-Sunflower_sky_backdrop.jpg",
-            function(res){
-                result = res;
-            }
-        );
-        waitsFor(function(){
-            return result;
-        }, 10000);
-        runs(function(){
-            expect(result.image).toBe("https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/250px-Sunflower_sky_backdrop.jpg");
+        FileExtension.resolve("http://example.com/", function( res ){
+            result = res;
         });
+        expect(result).toBe(null);
     });
 });
 
-describe("FileExtension : png", function(){
-    it("should return the png URL as is", function(){
+describe("FileExtension : jpg url", function() {
+    it("should return url", function(){
         var result;
-        imageresolver.resolve(
-            "http://www.libpng.org/pub/png/img_png/pnglogo-blk-sml1.png",
-            function(res){
-                result = res;
-            }
-        );
-        waitsFor(function(){
-            return result;
-        }, 10000);
-        runs(function(){
-            expect(result.image).toBe("http://www.libpng.org/pub/png/img_png/pnglogo-blk-sml1.png");
+        FileExtension.resolve("http://example.com/test.jpg", function( res ){
+            result = res;
         });
+        expect(result).toBe("http://example.com/test.jpg");
     });
 });
 
-describe("FileExtension : gif", function(){
-    it("should return the gif URL as is", function(){
+describe("FileExtension : jpg url with uppercase extension", function() {
+    it("should return url", function(){
         var result;
-        imageresolver.resolve(
-            "http://i.imgur.com/UmpOi.gif",
-            function(res){
-                result = res;
-            }
-        );
-        waitsFor(function(){
-            return result;
-        }, 10000);
-        runs(function(){
-            expect(result.image).toBe("http://i.imgur.com/UmpOi.gif");
+        FileExtension.resolve("http://example.com/test.JPG", function( res ){
+            result = res;
         });
+        expect(result).toBe("http://example.com/test.JPG");
+    });
+});
+
+
+describe("FileExtension : jpeg url", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.jpeg", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://example.com/test.jpeg");
+    });
+});
+
+describe("FileExtension : jpg url with fragment", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.jpg#fragment", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://example.com/test.jpg#fragment");
+    });
+});
+
+describe("FileExtension : jpg url with querystring", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.jpg?query=string", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://example.com/test.jpg?query=string");
+    });
+});
+
+describe("FileExtension : https jpg url", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("https://example.com/test.jpg", function( res ){
+            result = res;
+        });
+        expect(result).toBe("https://example.com/test.jpg");
+    });
+});
+
+describe("FileExtension : no protocol scheme jpg url", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("//example.com/test.jpg", function( res ){
+            result = res;
+        });
+        expect(result).toBe("//example.com/test.jpg");
+    });
+});
+
+describe("FileExtension : png url", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.png", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://example.com/test.png");
+    });
+});
+
+describe("FileExtension : gif url", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.gif", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://example.com/test.gif");
+    });
+});
+
+describe("FileExtension : bmp url", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.bmp", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://example.com/test.bmp");
+    });
+});
+
+describe("FileExtension : svg url", function() {
+    it("should return url", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.svg", function( res ){
+            result = res;
+        });
+        expect(result).toBe("http://example.com/test.svg");
+    });
+});
+
+describe("FileExtension : mp3 url", function() {
+    it("should return null", function(){
+        var result;
+        FileExtension.resolve("http://example.com/test.mp3", function( res ){
+            result = res;
+        });
+        expect(result).toBe(null);
     });
 });
